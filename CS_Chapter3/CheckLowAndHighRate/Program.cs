c1c20964c1841d46ecaf7835603efc04c9a07077
EAHSCS <EAHSCS>
2017-02-15 19:21:59 -0500

    Chapter 3 Finished Work

--- CS_Chapter3/CheckLowAndHighRate/Program.cs
+++ CS_Chapter3/CheckLowAndHighRate/Program.cs
@@ -9,6 +9,19 @@ namespace CheckLowAndHighRate
     {
         static void Main(string[] args)
         {
+            string payrateString, hourString;
+            double Payrate, Hours;
+            Console.WriteLine("Enter Payrate");
+            payrateString = Console.ReadLine();
+            Payrate = Convert.ToDouble(payrateString);
+            Console.WriteLine("Enter Hours Worked");
+            hourString = Console.ReadLine();
+            Hours = Convert.ToDouble(hourString);
+            
+            if (Payrate % Hours < 5.65 || Payrate % Hours > 50)
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
--- CS_Chapter3/CheckLowRate/Program.cs
+++ CS_Chapter3/CheckLowRate/Program.cs
@@ -9,6 +9,15 @@ namespace CheckLowRate
     {
         static void Main(string[] args)
         {
+            string payrateString;
+            double Payrate;
+            Console.WriteLine("Enter Payrate");
+            payrateString = Console.ReadLine();
+            Payrate = Convert.ToDouble(payrateString);
+            if (Payrate < 5.65 || Payrate > 50)
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
