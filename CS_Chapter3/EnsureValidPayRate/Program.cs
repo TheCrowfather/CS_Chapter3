d3dfbd2ffe3db083d847bb63fdae3daff665b9de
EAHSCS <EAHSCS>
2017-02-22 23:21:02 -0500

    Work

--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -7,9 +7,26 @@ namespace EnsureValidPayRate
 {
     class Program
     {
+        private static double rate;
+
         static void Main(string[] args)
         {
-
+            string payrateString, rateString;
+            double Payrate;
+            Console.WriteLine("Enter Payrate");
+            payrateString = Console.ReadLine();
+            Payrate = Convert.ToDouble(payrateString);
+            if (Payrate < 5.65 || Payrate > 50)
+            {
+                Console.WriteLine("Please enter payrate");
+              }
+            { 
+            if (Payrate < 5.65 || Payrate < 50)
+            Console.WriteLine("The weekly rate is:" + Payrate * 40);
+            rateString = Console.ReadLine();
+            rate = Convert.ToDouble(rateString);
+            }
+        
         }
     }
 }
